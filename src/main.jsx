74b2f898c5e06d7d4a01f51a5c3574f6fd4e5e00
freddymio <5839178+freddymio@users.noreply.github.com>
2025-05-06 15:32:31 +0200

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+// ENTRY POINT: main.jsx
+// PURPOSE: Entry for React app, renders App into the root element
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
